c8e1b5391e658f05ba94a53ef1337e270f29cb22
hamdihrd <hamdi.hrd@gmail.com>
2026-04-27 15:10:21 +0100

    version 1.2 sideBar Styled

--- script.js
+++ script.js
@@ -48,22 +48,43 @@ const createGameBoard = (size, nbreCellWin, player1, player2) => {
   };
 
   const detectWin = (i, j, xo) => {
+    let winCells = [];
     const getCount = (dx, dy) => {
       let count = 0;
       for (let step = 1; step < nbreCellWin; step++) {
         let x = i + dx * step;
         let y = j + dy * step;
-        if (!isValidRowCol(x, y) || board[x][y] !== xo) break;
+        const cell = document.getElementById(`${x}, ${y}`);
+
+        if (
+          !isValidRowCol(x, y) ||
+          board[x][y] !== xo ||
+          cell.classList.contains("win")
+        )
+          break;
         count++;
+        winCells.push([x, y]);
       }
 
       return count;
     };
 
     for (const direction of directions) {
+      winCells = [[i, j]];
       let [dx, dy] = direction;
-      let totalCount = 1 + getCount(dx, dy) + getCount(-dx, -dy);
-      if (totalCount >= nbreCellWin) return true;
+      let count1 = 1 + getCount(dx, dy);
+      let totalCount =
+        count1 >= nbreCellWin ? count1 : count1 + getCount(-dx, -dy);
+
+      if (totalCount >= nbreCellWin) {
+        for (let [x, y] of winCells) {
+          const cell = document.getElementById(`${x}, ${y}`);
+          cell.classList.add("win");
+          cell.style.backgroundColor =
+            xo === player1.mark ? "var(--bgwincolor1)" : "var(--bgwincolor2)";
+        }
+        return true;
+      }
     }
     return false;
   };
@@ -92,18 +113,22 @@ const createPlayer = (name, mark, getgameBoard) => {
   const getScore = () => {
     return score;
   };
+  const resetScore = () => {
+    score = 0;
+  };
 
   const play = (i, j) => {
     getgameBoard().setXO(i, j, mark);
   };
 
-  return { name, play, increaseScore, getScore, mark };
+  return { name, play, increaseScore, getScore, resetScore, mark };
 };
 
 function createMatrix(board) {
   let matrix = [];
 
   const grid = document.querySelector(".grid");
+  grid.replaceChildren();
   grid.style.gridTemplateColumns = `repeat(${board.length}, minmax(calc((min(100vh,calc( 100vw - 150px)) - 40px) / ${board.length}), 1fr))`;
   grid.style.gridTemplateRows = `repeat(${board.length}, minmax(calc((min(100vh,calc( 100vw - 150px)) - 40px) / ${board.length}), 1fr))`;
   for (let i = 0; i < board.length; i++) {
@@ -117,43 +142,70 @@ function createMatrix(board) {
   }
   grid.append(...matrix);
 }
-function reset(gameBoard) {
-  gameBoard.resetBoard();
-  createMatrix(gameBoard.getBoard());
-  player1.score = 0;
-  player2.score = 0;
-  document.querySelector(".score1").textContent = "SCORE = 0";
-  document.querySelector(".score2").textContent = "SCORE = 0";
-}
 
 function startGame() {
+  const start = document.querySelector(".start");
+  start.style.display = "none";
+  const restart = document.querySelector(".restart");
+  restart.style.display = "grid";
   const player1Name = document.querySelector("#player1Name").value;
   const player2Name = document.querySelector("#player2Name").value;
   const gridSize = parseInt(document.querySelector("#gridSize").value);
   const nbreCellWin = parseInt(document.querySelector("#nbreCellsWin").value);
+  if (
+    isNaN(gridSize) ||
+    isNaN(nbreCellWin) ||
+    player1Name === "" ||
+    player2Name === ""
+  ) {
+    alert("Invalid input");
+    return;
+  }
 
   let gameBoard;
   const grid = document.querySelector(".grid");
+  grid.replaceChildren();
 
   const player1 = createPlayer(player1Name, "✖️", () => gameBoard);
   const player2 = createPlayer(player2Name, "⭕", () => gameBoard);
   gameBoard = createGameBoard(gridSize, nbreCellWin, player1, player2);
   const resetButton = document.querySelector(".resetGame");
   resetButton.addEventListener("click", () => reset(gameBoard));
+  createMatrix(gameBoard.getBoard());
+  reset(gameBoard);
 
   let currentPlayer = player1;
+
+  function reset(gameBoard) {
+    gameBoard.resetBoard();
+    createMatrix(gameBoard.getBoard());
+    player1.resetScore();
+    player2.resetScore();
+    document.querySelector(".score1").textContent = "SCORE = 0";
+    document.querySelector(".score2").textContent = "SCORE = 0";
+  }
 
   grid.addEventListener("click", (e) => {
     if (e.target.classList.contains("cell")) {
       let [i, j] = e.target.id.split(",");
       i = parseInt(i);
       j = parseInt(j);
+      if (gameBoard.getBoard()[i][j] !== "") return;
       currentPlayer.play(i, j);
       currentPlayer = currentPlayer === player1 ? player2 : player1;
     }
   });
-  grid.innerHTML = "";
-  createMatrix(gameBoard.getBoard());
+  restart.addEventListener("click", () => {
+    start.style.display = "grid";
+    restart.style.display = "none";
+    document.querySelector("#player1Name").value = "";
+    document.querySelector("#player2Name").value = "";
+    document.querySelector("#gridSize").value = "";
+    document.querySelector("#nbreCellsWin").value = "";
+
+    reset(gameBoard);
+  });
 }
+
 const start = document.querySelector(".start");
 start.addEventListener("click", startGame);
